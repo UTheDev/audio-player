420d886027da17a9d5d360b549647193d6e8c1ac
udev <bees9633@gmail.com>
2023-05-30 23:19:33 -0700

    Update App.kt
    - fixed some exceptions
    - cancelling file selection (or selecting no files) should make the app exit properly now

--- src/main/kotlin/com/uthedev/audioplayer/App.kt
+++ src/main/kotlin/com/uthedev/audioplayer/App.kt
@@ -1,28 +1,29 @@
 package com.uthedev.audioplayer
 
 import javafx.application.Application
+import javafx.application.Platform
 import javafx.scene.Group
 import javafx.scene.Scene
 import javafx.scene.paint.Color
 import javafx.stage.Stage
 
 class App : Application() {
-    private lateinit var audioPlayer: AudioPlayer
+    private var audioPlayer: AudioPlayer? = null
 
     override fun start(stage: Stage) {
         // Prompt for a list of WAV files
         val fileList = WavChooser(stage).prompt()
 
-        if (fileList!!.size > 0) {
-            println("Received ${fileList!!.size} files from input. Now loading...")
+        if ((fileList != null) && (fileList.size > 0)) {
+            println("Received ${fileList.size} files from input. Now loading...")
             audioPlayer = AudioPlayer()
 
             for (file in fileList) {
-                audioPlayer.add(file.toURI().toURL())
+                audioPlayer!!.add(file.toURI().toURL())
             }
 
             val songList = SongList()
-            songList.update(audioPlayer)
+            songList.update(audioPlayer!!)
 
             //val fxmlLoader = FXMLLoader(App::class.java.getResource("hello-view.fxml"))
             val scene = Scene(Group(songList))//Scene(fxmlLoader.load(), 320.0, 240.0)
@@ -34,12 +35,13 @@ class App : Application() {
         } else {
             // Close immediately, as we've received no valid files from input
             stage.close()
+            Platform.exit()
         }
     }
 
     override fun stop() {
         if (audioPlayer != null) {
-            audioPlayer.clear()
+            audioPlayer!!.clear()
         }
     }
 }
